5a2208f1152e2d4b34fac38306e664f1c55fe042
Rashid usmani <rashidusmani506@gmail.com>
2026-06-08 11:30:49 +0530

    dictionary

--- hello.py
+++ hello.py
@@ -1,145 +1,156 @@
-# # a = int(input("Enter the first no. : "))
-# # b = int(input("Enter the second no. : "))
-# # operator = input("Enter the operator +, -, *, / : ")
-# # if operator == "+" : 
-# #     print(a+b)
-# # elif operator == "-" :
-# #     print(a-b)
-# # elif operator == "*":
-# #     print(a*b)
+# # # a = int(input("Enter the first no. : "))
+# # # b = int(input("Enter the second no. : "))
+# # # operator = input("Enter the operator +, -, *, / : ")
+# # # if operator == "+" : 
+# # #     print(a+b)
+# # # elif operator == "-" :
+# # #     print(a-b)
+# # # elif operator == "*":
+# # #     print(a*b)
 
-# # elif operator == "/" :
-# #     print(a/b)
+# # # elif operator == "/" :
+# # #     print(a/b)
 
-# # ## Second problem
+# # # ## Second problem
 
-# # num = int(input("Enter the input "))
-# # if num % 2 == 0:
-# #     print("No. is even",num)
-# # else :
-# #     print(num,"No. is odd")
+# # # num = int(input("Enter the input "))
+# # # if num % 2 == 0:
+# # #     print("No. is even",num)
+# # # else :
+# # #     print(num,"No. is odd")
 
-# # ## Third problem
-# # str = "Rashid"
-# # cout = 0
-# # vowel = "aeiouAEIOU"
-# # for i in range(0, len(str)):
-# #     for j in range(0, len(vowel)):
-# #         if str[i] == vowel[j]:
-# #             cout += 1
-# # print(cout)
+# # # ## Third problem
+# # # str = "Rashid"
+# # # cout = 0
+# # # vowel = "aeiouAEIOU"
+# # # for i in range(0, len(str)):
+# # #     for j in range(0, len(vowel)):
+# # #         if str[i] == vowel[j]:
+# # #             cout += 1
+# # # print(cout)
 
-# # ## fourth 
-# # arr = [88, 29, 90, 94, 64]
-# # largest = arr[0]
-# # second_largest = arr[0]
-# # for num in arr :
-# #     if num > largest :
-# #         second_largest = largest
-# #         largest = num
-# #     elif num > second_largest and num != largest :
-# #         second_largest = num
+# # # ## fourth 
+# # # arr = [88, 29, 90, 94, 64]
+# # # largest = arr[0]
+# # # second_largest = arr[0]
+# # # for num in arr :
+# # #     if num > largest :
+# # #         second_largest = largest
+# # #         largest = num
+# # #     elif num > second_largest and num != largest :
+# # #         second_largest = num
             
-# # print(largest, "Is the largest in array")
-# # print(second_largest, "Is the largest in array")
+# # # print(largest, "Is the largest in array")
+# # # print(second_largest, "Is the largest in array")
 
-# # ## sixth problem 
-# # positive = 0
-# # negative = 0
-# # zeros = 0
+# # # ## sixth problem 
+# # # positive = 0
+# # # negative = 0
+# # # zeros = 0
 
-# # arr2 = [5, -2, 0, 8, -1, 0, 3]
-# # for num in arr2 :
-# #     if num < 0 :
-# #         negative += 1
-# #     elif num > 0 :
-# #         positive += 1
+# # # arr2 = [5, -2, 0, 8, -1, 0, 3]
+# # # for num in arr2 :
+# # #     if num < 0 :
+# # #         negative += 1
+# # #     elif num > 0 :
+# # #         positive += 1
+# # #     else :
+# # #         zeros += 1
+# # # print(zeros)
+# # # print(positive)
+# # # print(negative)
+
+# # # ## seven problem 
+# # # arr3 = [1,2,22,33,43,44,54,38,94]
+# # # odd = 0
+# # # even = 0
+# # # for num in arr3:
+# # #     if num % 2 == 0 :
+# # #         even += 1
+# # #     else : 
+# # #         odd += 1 
+# # # print(odd)
+# # # print(even)
+
+# # ## 8th problem
+# # arr = [2,12,23,44,56]
+# # total = 0
+# # for num in arr :
+# #     total += num
+# # average = total/len(arr)
+# # print(average)
+
+# # ## 9th 
+
+# # count = 0
+# # name = "RaSHi64d AnW8593ar"
+# # for i in name:
+# #     if i.isdigit():
+# #         count += 1
+# # print(count)
+
+# # ## 10th
+
+# # nA = "level"
+# # reverse = ""
+# # for n in nA:
+# #     reverse = n + reverse
+# # if reverse == nA :
+# #     print("This is a palindrome")
+# # else :
+# #     print("This is not a palindrome")
+
+# # ## 15
+# # freq = {}
+# # count1 = 0
+# # str = "banana"
+# # for n in str:
+# #     if n in freq:
+# #         freq[n] += 1
 # #     else :
-# #         zeros += 1
-# # print(zeros)
-# # print(positive)
-# # print(negative)
-
-# # ## seven problem 
-# # arr3 = [1,2,22,33,43,44,54,38,94]
-# # odd = 0
-# # even = 0
-# # for num in arr3:
-# #     if num % 2 == 0 :
-# #         even += 1
-# #     else : 
-# #         odd += 1 
-# # print(odd)
-# # print(even)
-
-# ## 8th problem
-# arr = [2,12,23,44,56]
-# total = 0
-# for num in arr :
-#     total += num
-# average = total/len(arr)
-# print(average)
-
-# ## 9th 
-
-# count = 0
-# name = "RaSHi64d AnW8593ar"
-# for i in name:
-#     if i.isdigit():
-#         count += 1
-# print(count)
-
-# ## 10th
-
-# nA = "level"
-# reverse = ""
-# for n in nA:
-#     reverse = n + reverse
-# if reverse == nA :
-#     print("This is a palindrome")
-# else :
-#     print("This is not a palindrome")
-
-# ## 15
-# freq = {}
-# count1 = 0
-# str = "banana"
-# for n in str:
-#     if n in freq:
-#         freq[n] += 1
-#     else :
-#         freq[n] = 1
+# #         freq[n] = 1
         
 
-# print(freq)
+# # print(freq)
     
-# st = "rashid anwar"
-# # rev = ""
-# # for j in st:
-# #     if j == j.upper():
-# #         rev += j.lower() 
-# #     else :
-# #         rev += j.upper()
-# # print(rev)
+# # st = "rashid anwar"
+# # # rev = ""
+# # # for j in st:
+# # #     if j == j.upper():
+# # #         rev += j.lower() 
+# # #     else :
+# # #         rev += j.upper()
+# # # print(rev)
 
-# # he = ['rashid','anwar','usmani']
-# # print(he.capitalize())
+# # # he = ['rashid','anwar','usmani']
+# # # print(he.capitalize())
 
-# print(st.titel())
+# # print(st.titel())
 
-name = "hello   world  lol"
-ans = ""
+# name = "hello   world  lol"
+# ans = ""
 
-for i in name.split("") :
-    if i:
-        ans += i.capitalize()
-    ans += " "
-# name2 = name.split()
+# for i in name.split("") :
+#     if i:
+#         ans += i.capitalize()
+#     ans += " "
+# # name2 = name.split()
 
-# ans = []
+# # ans = []
 
-# for i in name2 :
-#     ans.append(i.capitalize())
-# print(" ".join(ans))
+# # for i in name2 :
+# #     ans.append(i.capitalize())
+# # print(" ".join(ans))
 
 
+n = 3
+x = 0
+y = 1 
+z = 2
+
+lis = [[i,j,k]
+        for i in range(x+1)
+        for j in range(y+1)
+        for k in range(z+1)
+       if i + j + k != n]
+print(lis)
---
+++ python_Basics/dictionary.py
@@ -0,0 +1,8 @@
+info = {
+    "name" : "Rashid",
+    "mentor" : "Shajar sir ",
+    "age" : 21,
+    "position" : "Intern"
+}
+info['age'] = 23
+print(info["age"])
