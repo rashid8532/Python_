e068bda8d9848a07e4d75dc5194d37433b0e8b4a
Rashid usmani <rashidusmani506@gmail.com>
2026-06-09 14:49:44 +0530

    test

--- hello.py
+++ hello.py
@@ -154,3 +154,20 @@ lis = [[i,j,k]
         for k in range(z+1)
        if i + j + k != n]
 print(lis)
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int,input().split()))
+N = int(input())
+for i in range(N):
+    x = input()
+    y = x.split()
+    if y[0] == "pop":
+        s.pop()
+    elif y[0] == "remove":
+        if int(y[1]) in s:
+            s.remove(int(y[1]))
+    elif y[0] == "discard" :
+        s.discard(int(y[1]))
+
+print(sum(s))  
--- test.py
+++ test.py
@@ -1,33 +1,9 @@
-n = int(input())
-s = set(map(int,input().split()))
-N = int(input())
-for i in range(N):
-    x = input()
-    y = x.split()
-    if y[0] == "pop":
-        s.pop()
-    elif y[0] == "remove":
-        if int(y[1]) in s:
-            s.remove(int(y[1]))
-    elif y[0] == "discard" :
-        s.discard(int(y[1]))
-
-print(sum(s))
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-s = set(map(int,input().split()))
-N = int(input())
-for i in range(N):
-    x = input()
-    y = x.split()
-    if y[0] == "pop":
-        s.pop()
-    elif y[0] == "remove":
-        if int(y[1]) in s:
-            s.remove(int(y[1]))
-    elif y[0] == "discard" :
-        s.discard(int(y[1]))
-
-print(sum(s))  
-s.un
+count = 0
+string = "ABCDCDC"
+sub_string = "CDC"
+for i in range(len(string)):
+    if string[i : len(sub_string)+i] == sub_string:
+        count += 1
+print(count)
+for i in range(2,10,2):
+    print(i)
