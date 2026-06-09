fe7b0caee1385d01f5997a18d8de508b142aad2a
Rashid usmani <rashidusmani506@gmail.com>
2026-06-09 18:15:43 +0530

    test

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-count = 0
-string = "ABCDCDC"
-sub_string = "CDC"
-for i in range(len(string)):
-    if string[i : len(sub_string)+i] == sub_string:
-        count += 1
-print(count)
-for i in range(2,10,2):
-    print(i)
-
+for i in range(3,0,-1):
+    print("*" * i)
+    
