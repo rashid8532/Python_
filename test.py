8c169fa8abeee746335ab015c80425694f710361
Rashid usmani <rashidusmani506@gmail.com>
2026-06-10 15:35:12 +0530

    test

--- test.py
+++ test.py
@@ -1,8 +1,34 @@
 for i in range(1,6):
-    for k in range(i,6):
-        print(" ", end='')
+    for l in range(i,6):
+        print(" ",end="")
     for j in range(i):
-        print("*",end='')
-    for j in range(i-1):
-        print("*",end='')
+        print("*",end="")
+    for k in range(i-1):
+        print("*",end="")
+    
     print()
+for i in range(2,6):
+    for l in range(i):
+        print(" ",end="")
+    for j in range(i,6):
+        print("*",end="")
+    for k in range(i+1,6):
+        print("*",end="")
+    
+    print()
+lis = []
+lis.append("j")
+print(lis)
+
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    s = [name,score]
+    lis = []
+    lis.append(s)
+lis = lis.sort()
+for i in lis:
+    if i[1][1] == i[2][1]:
+        print(i[1][1])
+        print(i[2][1])
+    
