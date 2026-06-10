6d51e1d9b4f246984cee7ebf4daaf2f8e39e77ad
Rashid usmani <rashidusmani506@gmail.com>
2026-06-10 15:00:52 +0530

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-for i in range(3,0,-1):
-    print("*" * i)
-    
+for i in range(1,6):
+    for k in range(i,6):
+        print(" ", end='')
+    for j in range(i):
+        print("*",end='')
+    for j in range(i-1):
+        print("*",end='')
+    print()
