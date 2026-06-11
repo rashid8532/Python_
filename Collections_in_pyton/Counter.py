032e00600e5d14e935e56b8d5f909e8bbcf98242
Rashid usmani <rashidusmani506@gmail.com>
2026-06-11 17:19:06 +0530

    Ordered

---
+++ Collections_in_pyton/Counter.py
@@ -0,0 +1,3 @@
+from collections import Counter
+d = Counter(list[1,2,4,5,3,4])
+print(d)
---
+++ Collections_in_pyton/OrderedDict.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+d = OrderedDict()
+d["A"] = 1
+d["B"] = 2
+d["C"] = 3
+
+print(d)
+for i , j in d.items():
+    print(i , j)
+
+d.move_to_end("A")
+print(d)
+lis = [1,2,3,4,5]
+print(lis[0 : -1])
+
+from collections import OrderedDict
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+d = OrderedDict()
+N = int(input())
+for i in range(N):
+    parts = input().split()
+    item = parts[0:-1]
+    price = parts[-1]
+    d[item] = price
+print(d)
+    
