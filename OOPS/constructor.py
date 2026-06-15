00079de217a604e347a357b8b7f17ed3f9bdf168
Rashid usmani <rashidusmani506@gmail.com>
2026-06-15 15:44:26 +0530

    oop

---
+++ OOPS/constructor.py
@@ -0,0 +1,9 @@
+class Student :
+    def __init__(self,name):
+        self.name =""
+        self.name = name
+s1 = Student("Rashid")
+print(s1.name)
+s2 = Student("")
+s1.name = "Akmal"
+print(s1.name)
