4662e1e9afc9776b2551e62d2fc7de4e3fc0c2e8
Rashid usmani <rashidusmani506@gmail.com>
2026-07-07 16:34:37 +0530

    Bubble sort

--- DSA_PYTHON/07_Searching.py
+++ DSA_PYTHON/07_Searching.py
@@ -58,16 +58,3 @@ def is_sorted(arr):
  
 print(is_sorted(arr2))
 
-def Bubble_sort(arr):
-    flag = 0
-    for i in range(len(arr) - 1) :
-        flag = 0
-        print("pass ",i)
-        for j in range(len(arr) - 1 -i):
-            if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
-                flag = 1
-        if flag == 0 :
-            return arr
-    return arr
-print(Bubble_sort([6,1,2,3,4,5]))
